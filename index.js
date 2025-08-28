4e8bd9714656e420a52f5259c3c3789fcc7f57ec
josephineeho <josephineeho8@gmail.com>
2025-08-28 08:50:38 +0100

    My first project given to me by my supervisor at the internship

---
+++ index.js
@@ -0,0 +1,12 @@
+const textContent = document.getElementById("textContent");
+const numbsVarry = document.getElementById("numbsVarry");
+const minusBtn = document.getElementById("minusBtn");
+const plusBtn = document.getElementById("plusBtn");
+const border = document.getElementById("border");
+const startBtn = document.getElementById("startBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+
+function calc() {
+    if
+}
