305143328152cc96d947f7cdda0bbc5ad6d6cda5
josephineeho <josephineeho8@gmail.com>
2025-09-04 03:14:58 +0100

    This is what i presented to at Go-Groups LTD

--- index.js
+++ index.js
@@ -8,60 +8,109 @@ const stopBtn = document.getElementById("stopBtn")
 const resetBtn = document.getElementById("resetBtn");
 stopBtn.classList.add("hide");
 startBtn.classList.add("show");
-let count = 0;
 let isRunning = false;
+let seconds = 0;
+var minutes = 0;
+let increment = 1;
+let interval = null;
+var hours = 0;
 
 
 plusBtn.onclick = function() {  
-            count++;  
-    if(count <= 10) {
-    numbsVarry.textContent = count;
-    }
-    else{
-      return  count = 0;
-    }
+  if(increment <10){
+    increment++;
+  }
+  else{
+    increment = 0;
+  }
+  numbsVarry.textContent = increment;
+    };
     
-}
+
 minusBtn.onclick = function() {
-    if(count <= 10 && count !=0) {
-    count--;
-numbsVarry.textContent = count;
+    if(increment >0){
+        increment--;
+    }
+    numbsVarry.textContent = increment;
 }
-return 0;
-}
+
+// START BUTTON
 
 startBtn.onclick = function() {
-    toggleButton();
-    let count = 0 ;
-    textLabel.textContent = count;
-    const interval = setInterval(() =>
-    {
-        count++;
-        textLabel.textContent = count;
-        if(count >= 60){
-            clearInterval(interval);
-        }
-    }, 200);
-}
-resetBtn.onclick = function() {
-    if(count > 0)
-    count =0;
-    textLabel.textContent = count;
-}
-
-function toggleButton( ){
-if(!isRunning){
-    startBtn.classList.add("hide");
-    stopBtn.classList.remove("hide");
+    if(!isRunning){
+        let addBy = parseInt(numbsVarry.textContent);
+        isRunning = true;
+        startBtn.classList.add("hide");
+        stopBtn.classList.remove("hide");
     stopBtn.classList.add("show");
     startBtn.classList.remove("show");
-    isRunning = true;
-}
-else{
+        // let increment= parseInt(numbsVarry.textContent)||1;
+
+        interval = setInterval(()=>{
+            seconds+=addBy || 1;
+            if(seconds>=60){
+                seconds=0;
+                minutes++;
+            }
+            if(minutes>=60){
+                minutes=0;
+                hours++;
+            }
+            textLabel.textContent=pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+        },200);
+    }
+};
+stopBtn.onclick = function() {
+    if(isRunning){
+        startBtn.classList.remove("hide");
+        startBtn.classList.add("show");
+        stopBtn.classList.remove("show");
+        stopBtn.classList.add("hide");
+        isRunning= false;
+        clearInterval(interval);
+    }
+};
+
+resetBtn.onclick = function() {
+    //  toggleButton();
+    clearInterval(interval);
+
+    isRunning = false;
     startBtn.classList.remove("hide");
     startBtn.classList.add("show");
     stopBtn.classList.remove("show");
-    stopBtn.classList.add("hide")
-    isRunning = false;
+    stopBtn.classList.add("hide");
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    textLabel.textContent = "00:00:00";
+
 }
-    }``
+
+// PAD NUMBERS
+function pad(num) {
+    return String(num).padStart(2, "0");
+}
+
+// function toggleButton( ){
+// if(!isRunning){
+//     startBtn.classList.add("");
+//     stopBtn.classList.remove("hide");
+//     stopBtn.classList.add("show");
+//     startBtn.classList.remove("show");
+//     // isRunning = true;
+// }
+// else if(resetBtn.onclick){
+//     startBtn.classList.remove("hide");
+//     startBtn.classList.add("show");
+//     stopBtn.classList.remove("show");
+//     stopBtn.classList.add("hide");
+// }
+// else{
+//     startBtn.classList.remove("hide");
+//     startBtn.classList.add("show");
+//     stopBtn.classList.remove("show");
+//     stopBtn.classList.add("hide")
+//     // isRunning = false;
+// }
+//     }
